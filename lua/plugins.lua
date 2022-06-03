7519050d432a78cc2006c6b74dd46918c0addcf5
Matthew Lee <mattxlee@gmail.com>
2022-06-03 17:58:40 +0800

    Improve layouts.

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,7 +5,15 @@ return require('packer').startup({function()
 
     use 'morhetz/gruvbox'
 
-    use 'liuchengxu/eleline.vim'
+    use 'ryanoasis/vim-devicons'
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+        config = function()
+            require('lualine').setup()
+        end
+    }
 
     use 'editorconfig/editorconfig-vim'
 
@@ -50,6 +58,10 @@ return require('packer').startup({function()
     use 'vim-scripts/DoxygenToolkit.vim'
 
     use 'preservim/nerdtree'
+
+    use 'Xuyuanp/nerdtree-git-plugin'
+
+    use 'vwxyutarooo/nerdtree-devicons-syntax'
 
     use 'godlygeek/tabular'
 
