3313f247ed6ec63c5826d16fe84093fd0c3ac71a
Matthew Lee <mattxlee@gmail.com>
2024-05-03 16:13:57 +0000

    Update font styles.

--- lua/conf_theme.lua
+++ lua/conf_theme.lua
@@ -16,11 +16,11 @@ require('catppuccin').setup({
     no_bold = false, -- Force no bold
     no_underline = false, -- Force no underline
     styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
-        comments = {}, -- Change the style of comments
-        conditionals = {},
+        comments = { 'italic' }, -- Change the style of comments
+        conditionals = { 'bold' },
         loops = {},
-        functions = {},
-        keywords = {},
+        functions = { 'italic' },
+        keywords = { 'bold' },
         strings = {},
         variables = {},
         numbers = {},
