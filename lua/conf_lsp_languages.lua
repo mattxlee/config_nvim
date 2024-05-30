2aed545efdf4413aa1194bc60d00f696015eddae
Matthew Lee <mattxlee@gmail.com>
2024-05-31 01:06:43 +0800

    Change the shortcut for Mason.

--- lua/conf_lsp_languages.lua
+++ lua/conf_lsp_languages.lua
@@ -15,7 +15,7 @@ require('mason').setup()
 require('mason-lspconfig').setup()
 
 vim.keymap.set('n', '<c-h>', ':ClangdSwitchSourceHeader<CR>')
-vim.keymap.set('n', '<leader>c', ':Mason<CR>')
+vim.keymap.set('n', '<c-;>', ':Mason<CR>')
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
