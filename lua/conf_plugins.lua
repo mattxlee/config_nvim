6fd34905db542ce2886074de852ffff4904b3dc4
Matthew Lee <mattxlee@gmail.com>
2024-05-31 01:15:07 +0800

    Load treesitter: 'markdown' in default.

--- lua/conf_plugins.lua
+++ lua/conf_plugins.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup({
     -- A list of parser names, or 'all'
-    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query' },
+    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'markdown' },
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
     -- List of parsers to ignore installing (for 'all')
