4c0fd0d87d0653d653c7236a1ec9cb30ba17717a
MATTHΞW <mattxlee@gmail.com>
2024-08-01 04:39:21 +0800

    Improved lsp diagnostic icons from column line.

--- lua/conf_lsp_languages.lua
+++ lua/conf_lsp_languages.lua
@@ -11,10 +11,8 @@ local on_attach = function(client, bufnr)
     vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
 end
 
-require('mason').setup()
-require('mason-lspconfig').setup()
-
 vim.keymap.set('n', '<leader>m', ':Mason<CR>')
+require('mason').setup()
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
@@ -37,16 +35,26 @@ require('flutter-tools').setup({
     }
 })
 
--- common settings
-local setup_entries = {
-    on_attach = on_attach,
-    capabilities = capabilities,
-}
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here
+  -- with the ones you want to install
+  ensure_installed = {},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+      })
+    end,
+  }
+})
 
-local langs = { 'cmake', 'rust_analyzer', 'texlab', 'gopls', 'tsserver', 'tailwindcss',  'cssls', 'pylsp'}
-for _, lang in ipairs(langs) do
-    require('lspconfig')[lang].setup(setup_entries)
-end
+vim.cmd [[
+sign define DiagnosticSignError text=  linehl= texthl=DiagnosticSignError numhl=
+sign define DiagnosticSignWarn text= linehl= texthl=DiagnosticSignWarn numhl=
+sign define DiagnosticSignInfo text=  linehl= texthl=DiagnosticSignInfo numhl=
+sign define DiagnosticSignHint text=💡  linehl= texthl=DiagnosticSignHint numhl=
+]]
 
 -- fix: renaming buffer newer than edits
 vim.lsp.util.apply_text_document_edit = function(text_document_edit, index, offset_encoding)
