b4278311421edf44d2ee05db2d07dc414e72e184
MATTHΞW <mattxlee@gmail.com>
2024-11-19 16:20:01 +0000

    Remove fade in/out and set the timeout to 10 seconds for nvim-notify.

--- init.lua
+++ init.lua
@@ -86,14 +86,27 @@ require('lazy').setup({
             -- add any options here
         },
         dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+            -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
             'MunifTanjim/nui.nvim',
             -- OPTIONAL:
             --   `nvim-notify` is only needed, if you want to use the notification view.
             --   If not available, we use `mini` as the fallback
             'rcarriga/nvim-notify',
         }
-    }
+    },
+    -- Custom the settings of nvim-notify
+    {
+        'dapetcu21/nvim-notify',
+        config = function ()
+            local notify = require('notify')
+            notify.setup({
+                render = 'wrapped-compact',
+                stages = 'static', -- <==== This is the workaround
+                timeout = 10000,
+            })
+            vim.notify = notify
+        end,
+    },
 })
 
 -- common setup --
