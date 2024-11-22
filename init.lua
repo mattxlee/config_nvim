e8362d198f343cd89edbe9e01ba3f628fda4e4d4
MATTHΞW <mattxlee@gmail.com>
2024-11-22 09:01:15 +0000

    Remove noice, it makes things more complex.

--- init.lua
+++ init.lua
@@ -78,35 +78,6 @@ require('lazy').setup({
     { 'lewis6991/gitsigns.nvim' },
     -- Fugitive
     { 'tpope/vim-fugitive' },
-    -- Notice replacement
-    {
-        'folke/noice.nvim',
-        event = 'VeryLazy',
-        opts = {
-            -- add any options here
-        },
-        dependencies = {
-            -- if you lazy-load any plugin below, make sure to add proper `module='...'` entries
-            'MunifTanjim/nui.nvim',
-            -- OPTIONAL:
-            --   `nvim-notify` is only needed, if you want to use the notification view.
-            --   If not available, we use `mini` as the fallback
-            'rcarriga/nvim-notify',
-        }
-    },
-    -- Custom the settings of nvim-notify
-    {
-        'dapetcu21/nvim-notify',
-        config = function ()
-            local notify = require('notify')
-            notify.setup({
-                render = 'wrapped-compact',
-                stages = 'static',
-                timeout = 3000,
-            })
-            vim.notify = notify
-        end,
-    },
 })
 
 -- common setup --
