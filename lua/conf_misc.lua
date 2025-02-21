e1f411ec80bfff7e00f22493f80578932012948d
MATTHΞW <mattxlee@gmail.com>
2025-02-21 15:36:10 +0000

    feat: add RainbowGray highlight for indent lines
    - Changed highlight from 'whitespace' to 'RainbowGray'
    - Added hooks to reset highlight groups when colorscheme changes
    - Set 'RainbowGray' foreground color to '#333333'

--- lua/conf_misc.lua
+++ lua/conf_misc.lua
@@ -138,8 +138,14 @@ require('nvim-surround').setup()
 
 -- Settings for indent lines
 local highlight = {
-    'whitespace',
+    'RainbowGray',
 }
+local hooks = require 'ibl.hooks'
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, 'RainbowGray', { fg = '#333333' })
+end)
 require('ibl').setup({
     indent = { highlight = highlight },
     scope = { enabled = false },
