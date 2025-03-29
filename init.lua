62265b1285a379a72c7a9143cb3d75a51ec6f2b1
MATTHΞW <mattxlee@gmail.com>
2025-03-30 00:48:30 +0800

    feat(theme): switch from GitHub theme to Gruvbox
    - Replace GitHub theme with Gruvbox nvim theme
    - Configure Gruvbox with dark background and custom styling options
    - Move theme configuration to conf_misc.lua for better organization

--- init.lua
+++ init.lua
@@ -22,60 +22,8 @@ require('lazy').setup({
         border = 'rounded',
     },
     spec = {
-        -- theme
-        {
-            'projekt0n/github-nvim-theme',
-            name = 'github-theme',
-            lazy = false, -- make sure we load this during startup if it is your main colorscheme
-            priority = 1000, -- make sure to load this before all the other start plugins
-            config = function()
-                require('github-theme').setup({
-                    options = {
-                        -- Compiled file's destination location
-                        compile_path = vim.fn.stdpath('cache') .. '/github-theme',
-                        compile_file_suffix = '_compiled', -- Compiled file suffix
-                        hide_end_of_buffer = true, -- Hide the '~' character at the end of the buffer for a cleaner look
-                        hide_nc_statusline = true, -- Override the underline style for non-active statuslines
-                        transparent = false,       -- Disable setting bg (make neovim's background transparent)
-                        terminal_colors = true,    -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-                        dim_inactive = false,      -- Non focused panes set to alternative background
-                        module_default = true,     -- Default enable value for modules
-                        styles = {                 -- Style to be applied to different syntax groups
-                            comments = 'italic',       -- Value is any valid attr-list value `:help attr-list`
-                            functions = 'bold',
-                            keywords = 'bold',
-                            variables = 'NONE',
-                            conditionals = 'NONE',
-                            constants = 'NONE',
-                            numbers = 'NONE',
-                            operators = 'NONE',
-                            strings = 'italic',
-                            types = 'NONE',
-                        },
-                        inverse = {                -- Inverse highlight for different types
-                            match_paren = false,
-                            visual = false,
-                            search = false,
-                        },
-                        darken = {                 -- Darken floating windows and sidebar-like windows
-                            floats = true,
-                            sidebars = {
-                                enable = true,
-                                list = {},             -- Apply dark background to specific windows
-                            },
-                        },
-                        modules = {                -- List of various plugins and additional options
-                            -- ...
-                        },
-                    },
-                    palettes = {},
-                    specs = {},
-                    groups = {},
-                })
-                vim.cmd('colorscheme github_light_default')
-                vim.cmd('hi NormalFloat guibg=NONE')
-            end,
-        },
+        -- gruvbox theme
+        { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true },
         -- cursor
         { "sphamba/smear-cursor.nvim", opts = {
             -- Smear cursor when switching buffers or windows.
--- lua/conf_misc.lua
+++ lua/conf_misc.lua
@@ -1,3 +1,31 @@
+-- Default options:
+require('gruvbox').setup({
+  terminal_colors = true, -- add neovim terminal colors
+  undercurl = true,
+  underline = true,
+  bold = true,
+  italic = {
+    strings = true,
+    emphasis = false,
+    comments = true,
+    operators = false,
+    folds = true,
+  },
+  strikethrough = true,
+  invert_selection = false,
+  invert_signs = false,
+  invert_tabline = false,
+  invert_intend_guides = false,
+  inverse = true, -- invert background for search, diffs, statuslines and errors
+  contrast = '', -- can be "hard", "soft" or empty string
+  palette_overrides = {},
+  overrides = {},
+  dim_inactive = false,
+  transparent_mode = false,
+})
+vim.cmd('set background=dark')
+vim.cmd('colorscheme gruvbox')
+
 -- Treesitter for highlighting keywords, functions and etc
 require('nvim-treesitter.configs').setup({
     -- A list of parser names, or 'all'
