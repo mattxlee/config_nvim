e622c5c612b8c39fc9677883f2c7403940af8eae
MATTHΞW <mattxlee@gmail.com>
2025-04-05 20:05:16 +0800

    fix: quit NvmTree if it is the latest window.

--- lua/conf_misc.lua
+++ lua/conf_misc.lua
@@ -72,6 +72,14 @@ end
 require('nvim-tree').setup({
     on_attach = my_on_attach,
 })
+vim.o.confirm = true
+vim.api.nvim_create_autocmd('BufEnter', {
+    group = vim.api.nvim_create_augroup('NvimTreeClose', {clear = true}),
+    callback = function()
+        local layout = vim.api.nvim_call_function('winlayout', {})
+        if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), 'filetype') == 'NvimTree' and layout[3] == nil then vim.cmd('quit') end
+    end
+})
 vim.keymap.set('n', '<c-j>', ':NvimTreeFindFile<CR>')
 vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
 
