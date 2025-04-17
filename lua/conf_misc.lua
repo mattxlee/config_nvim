cea1aa520d68022081b4f3c7b77f9cefa54b929e
MATTHΞW <mattxlee@gmail.com>
2025-04-17 13:37:20 +0800

    feat: use oil instead of nvim-tree.

--- lua/conf_misc.lua
+++ lua/conf_misc.lua
@@ -64,24 +64,15 @@ vim.filetype.add({
 vim.treesitter.language.register('html', 'ejs')
 
 -- file tree explorer
-local function my_on_attach(bufnr)
-    local api = require('nvim-tree.api')
-    api.config.mappings.default_on_attach(bufnr)
-    vim.keymap.del('n', '<C-e>', { buffer = bufnr })
-end
-require('nvim-tree').setup({
-    on_attach = my_on_attach,
+require('oil').setup({
+    columns = {
+        'icon',
+        'permissions',
+        'size',
+        'mtime',
+    },
 })
-vim.o.confirm = true
-vim.api.nvim_create_autocmd('BufEnter', {
-    group = vim.api.nvim_create_augroup('NvimTreeClose', {clear = true}),
-    callback = function()
-        local layout = vim.api.nvim_call_function('winlayout', {})
-        if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), 'filetype') == 'NvimTree' and layout[3] == nil then vim.cmd('quit') end
-    end
-})
-vim.keymap.set('n', '<c-j>', ':NvimTreeFindFile<CR>')
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+vim.keymap.set('n', '<c-j>', ':Oil<CR>')
 
 -- Status bar
 local opts = {
